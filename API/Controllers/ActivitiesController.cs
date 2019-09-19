27920df1f4f7303e85e58085aa6c5250569d2489
pece55 <peceveljanovski1@gmail.com>
2019-09-19 11:58:21 +0200

    Some loaders and Axious..Done!

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -24,22 +24,26 @@ namespace API.Controllers
         {
             return await _mediator.Send(new List.Query());
         }
-        [HttpGet("id")]
+
+        [HttpGet("{id}")]
         public async Task<ActionResult<Activity>> Details(Guid id)
         {
             return await _mediator.Send(new Details.Query { Id = id });
         }
+
         [HttpPost]
         public async Task<ActionResult<Unit>> Create(Create.Command command)
         {
             return await _mediator.Send(command);
         }
-        [HttpPut]
-        public async Task<ActionResult<Unit>> Edit(Guid id, Create.Command command)
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Unit>> Edit(Guid id, Edit.Command command)
         {
             command.Id = id;
             return await _mediator.Send(command);
         }
+
         [HttpDelete("{id}")]
         public async Task<ActionResult<Unit>> Delete(Guid id)
         {
